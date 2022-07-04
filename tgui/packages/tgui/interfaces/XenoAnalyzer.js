c149a6a2f00b9d7a0d9b5f265bef69ef109dc989
Jolly-66 <70232195+Jolly-66@users.noreply.github.com>
2022-07-04 16:50:59 -0400

    tgui

--- tgui/packages/tgui/interfaces/XenoAnalyzer.js
+++ tgui/packages/tgui/interfaces/XenoAnalyzer.js
@@ -32,30 +32,60 @@ export const XenoAnalyzer = (props, context) => {
                 <center>
                   <Section height="320" backgroundColor="black">
                     <img
-                      src={"data:image/jpeg;base64, " + plant_appearance}
+                      src={'data:image/jpeg;base64, ' + plant_appearance}
                       width="100%"
                       height="100%"
                       style={{
                         '-ms-interpolation-mode': 'nearest-neighbor',
-                      }} />
+                      }}
+                    />
                   </Section>
                 </center>
-                <Icon name="lightbulb" size={2} color={light_level ? "green" : "red"} /><font size="+2"> Lit</font><br />
-                <Icon name="circle" size={2} color={researched ? "green" : "red"} /> <font size="+2"> Researched</font><br />
+                <Icon
+                  name="lightbulb"
+                  size={2}
+                  color={light_level ? 'green' : 'red'}
+                />
+                <font size="+2"> Lit</font>
+                <br />
+                <Icon
+                  name="circle"
+                  size={2}
+                  color={researched ? 'green' : 'red'}
+                />{' '}
+                <font size="+2"> Researched</font>
+                <br />
               </Section>
             </Table.Cell>
             <Table.Cell>
               <Section title={plant_name}>
-                Health: <ProgressBar value={health} height={2} maxValue={100} /><br />
-                Water level: <ProgressBar value={water_level} height={2} maxValue={max_water}>{water_level} units</ProgressBar><br />
-                Instability: <ProgressBar value={instability * 0.01} height={2} maxValue={100} /><br />
-                Fertilizer: <ProgressBar value={fertilizer} height={2} maxValue={max_nutri}>{fertilizer} units</ProgressBar><br />
-                Pests: <ProgressBar value={pests} height={2} maxValue={10} /><br />
+                Health: <ProgressBar value={health} height={2} maxValue={100} />
+                <br />
+                Water level:{' '}
+                <ProgressBar
+                  value={water_level}
+                  height={2}
+                  maxValue={max_water}>
+                  {water_level} units
+                </ProgressBar>
+                <br />
+                Instability:{' '}
+                <ProgressBar
+                  value={instability * 0.01}
+                  height={2}
+                  maxValue={100}
+                />
+                <br />
+                Fertilizer:{' '}
+                <ProgressBar value={fertilizer} height={2} maxValue={max_nutri}>
+                  {fertilizer} units
+                </ProgressBar>
+                <br />
+                Pests: <ProgressBar value={pests} height={2} maxValue={10} />
+                <br />
               </Section>
               <Section title="Description">
-                <font size="+2">
-                  {plant_desc}
-                </font>
+                <font size="+2">{plant_desc}</font>
               </Section>
             </Table.Cell>
           </Table>
