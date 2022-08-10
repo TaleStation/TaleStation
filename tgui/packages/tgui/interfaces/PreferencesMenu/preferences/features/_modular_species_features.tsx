da947cf78f670f22ca8cceaa0be994a54922dbc5
Jolly <70232195+Jolly-66@users.noreply.github.com>
2022-08-10 15:39:21 -0400

    guh (#1923)

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/_modular_species_features.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/_modular_species_features.tsx
@@ -1,11 +1,6 @@
-import { CheckboxInput, FeatureChoiced, FeatureDropdownInput, FeatureToggle } from './base';
+import { FeatureChoiced, FeatureDropdownInput } from './base';
 
 export const feature_head_tentacles: FeatureChoiced = {
   name: 'Head Tentacles',
   component: FeatureDropdownInput,
-};
-
-export const hair_lizard: FeatureToggle = {
-  name: 'Hair Lizard',
-  component: CheckboxInput,
 };
