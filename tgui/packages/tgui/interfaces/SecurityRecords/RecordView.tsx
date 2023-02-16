241495e3109c77d5070139025bb95901fd67b159
TaleStationBot <101908892+TaleStationBot@users.noreply.github.com>
2023-02-16 17:20:24 -0500

    [MIRROR] [MDB IGNORE] Records hotfix [NO GBP] (#4526)
    Original PR: https://github.com/tgstation/tgstation/pull/73363
    -----
    ## About The Pull Request
    A remake of #73169
    Repaths records consoles to their own subtype
    Fixes some weirdness with observers logging into sec/med consoles
    Fixes security notes not being able to be set etc
    
    ## Why It's Good For The Game
    Bug fixes and cleaner code
    
    ## Changelog
    :cl:
    fix: Security can set notes through consoles again
    fix: Observers are now properly logged out of record consoles
    /:cl:
    
    ---------
    
    Co-authored-by: Jeremiah <42397676+jlsnow301@users.noreply.github.com>
    Co-authored-by: Jolly-66 <70232195+Jolly-66@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/SecurityRecords/RecordView.tsx
+++ tgui/packages/tgui/interfaces/SecurityRecords/RecordView.tsx
@@ -1,11 +1,11 @@
 import { useBackend, useLocalState } from 'tgui/backend';
-import { NoticeBox, Stack, Section, Button, LabeledList, Box, RestrictedInput, Table } from 'tgui/components';
+import { Box, Button, LabeledList, NoticeBox, RestrictedInput, Section, Stack, Table } from 'tgui/components';
 import { CharacterPreview } from '../common/CharacterPreview';
 import { EditableText } from '../common/EditableText';
-import { CRIMESTATUS2COLOR, CRIMESTATUS2DESC } from './constants';
 import { CrimeWatcher } from './CrimeWatcher';
-import { getSecurityRecord } from './helpers';
 import { RecordPrint } from './RecordPrint';
+import { CRIMESTATUS2COLOR, CRIMESTATUS2DESC } from './constants';
+import { getSecurityRecord } from './helpers';
 import { SecurityRecordsData } from './types';
 
 /** Views a selected record. */
@@ -171,7 +171,11 @@ const RecordInfo = (props, context) => {
               />
             </LabeledList.Item>
             <LabeledList.Item label="Note">
-              <EditableText field="note" target_ref={crew_ref} text={note} />
+              <EditableText
+                field="security_note"
+                target_ref={crew_ref}
+                text={note}
+              />
             </LabeledList.Item>
             {
               // NON-MODULAR CHANGES: Adds sec records to TGUI
