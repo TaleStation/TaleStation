9c4c6dfcbb7f88b2272dc9f82e86969f524f36e5
Jolly-66 <70232195+Jolly-66@users.noreply.github.com>
2023-05-17 16:26:13 -0400

    extended info

--- tgui/packages/tgui/interfaces/Changelog.js
+++ tgui/packages/tgui/interfaces/Changelog.js
@@ -176,26 +176,33 @@ export class Changelog extends Component {
 
     const header = (
       <Section>
-        <h1>Traditional Games Space Station 13</h1>
+        <h1>TaleStation</h1>
         <p>
           <b>Thanks to: </b>
-          Baystation 12, /vg/station, NTstation, CDK Station devs,
+          /tg/station, Baystation 12, /vg/station, NTstation, CDK Station devs,
           FacepunchStation, GoonStation devs, the original Space Station 13
           developers, Invisty for the title image and the countless others who
           have contributed to the game, issue tracker or wiki over the years.
         </p>
         <p>
+          We would like to thank the original contirbutors from the original
+          JollyStation, now MapleStation, for their hardwork and for helping
+          building this codebase to what it is today. More importantly, we thank
+          MrMelbert immensely for their hardwork for many of the systems this
+          codebase uses.
+        </p>
+        <p>
           {'Current organization members can be found '}
-          <a href="https://github.com/orgs/tgstation/people">here</a>
+          <a href="https://github.com/orgs/TaleStation/people">here</a>
           {', recent GitHub contributors can be found '}
-          <a href="https://github.com/tgstation/tgstation/pulse/monthly">
+          <a href="https://github.com/TaleStation/TaleStation/pulse/monthly">
             here
           </a>
           .
         </p>
         <p>
           {'You can also join our discord '}
-          <a href="https://tgstation13.org/phpBB/viewforum.php?f=60">here</a>.
+          <a href="https://discord.gg/24Q2rKzgR3">here</a>.
         </p>
         {dateDropdown}
       </Section>
