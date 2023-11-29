531bfec2b7a8f35ef1a01b60017d7299b920775e
TaleStationBot <101908892+TaleStationBot@users.noreply.github.com>
2023-11-29 17:12:14 -0500

    [MIRROR] [MDB IGNORE] Removes context from tgui (#8837)
    Original PR: https://github.com/tgstation/tgstation/pull/80003
    -----
    
    ## About The Pull Request
    *Most context
    This is a precursor PR for moving to React. Newer React (>16) does not
    have the `context` parameter, so it needs removed. I want to make
    converting to React look slightly less intimidating, so this is just
    short-circuiting context here.
    
    This shouldn't be considered a long term solution.
    ## Why It's Good For The Game
    Moving to react is good actually
    ## Changelog
    N/A hopefully no noticeable change
    
    ---------
    
    Co-authored-by: Jeremiah <42397676+jlsnow301@users.noreply.github.com>
    Co-authored-by: Jolly-66 <70232195+Jolly-66@users.noreply.github.com>

--- tgui/packages/tgui/index.tsx
+++ tgui/packages/tgui/index.tsx
@@ -28,7 +28,7 @@ import './styles/themes/wizard.scss';
 import './styles/themes/jolly-syndicate.scss'; // NON-MODULAR CHANGES
 import './styles/themes/admin.scss';
 
-import { StoreProvider, configureStore } from './store';
+import { configureStore } from './store';
 
 import { captureExternalLinks } from './links';
 import { createRenderer } from './renderer';
@@ -36,6 +36,7 @@ import { perf } from 'common/perf';
 import { setupGlobalEvents } from './events';
 import { setupHotKeys } from './hotkeys';
 import { setupHotReloading } from 'tgui-dev-server/link/client.cjs';
+import { setGlobalStore } from './backend';
 
 perf.mark('inception', window.performance?.timing?.navigationStart);
 perf.mark('init');
@@ -43,13 +44,11 @@ perf.mark('init');
 const store = configureStore();
 
 const renderApp = createRenderer(() => {
+  setGlobalStore(store);
+
   const { getRoutedComponent } = require('./routes');
   const Component = getRoutedComponent(store);
-  return (
-    <StoreProvider store={store}>
-      <Component />
-    </StoreProvider>
-  );
+  return <Component />;
 });
 
 const setupApp = () => {
