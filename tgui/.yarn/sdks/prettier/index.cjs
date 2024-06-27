ef92af6199bcbf7f5fb4b2850665080593560904
Jolly-66 <70232195+Jolly-66@users.noreply.github.com>
2024-06-27 17:07:22 -0400

    undo all of that

--- tgui/.yarn/sdks/prettier/index.cjs
+++ tgui/.yarn/sdks/prettier/index.cjs
@@ -1,25 +1,18 @@
 #!/usr/bin/env node
 
-const {existsSync} = require(`fs`);
-const {createRequire, register} = require(`module`);
-const {resolve} = require(`path`);
-const {pathToFileURL} = require(`url`);
+const { existsSync } = require(`fs`);
+const { createRequire } = require(`module`);
+const { resolve } = require(`path`);
 
-const relPnpApiPath = "../../../.pnp.cjs";
+const relPnpApiPath = '../../../.pnp.cjs';
 
 const absPnpApiPath = resolve(__dirname, relPnpApiPath);
 const absRequire = createRequire(absPnpApiPath);
-
-const absPnpLoaderPath = resolve(absPnpApiPath, `../.pnp.loader.mjs`);
-const isPnpLoaderEnabled = existsSync(absPnpLoaderPath);
 
 if (existsSync(absPnpApiPath)) {
   if (!process.versions.pnp) {
     // Setup the environment to be able to require prettier
     require(absPnpApiPath).setup();
-    if (isPnpLoaderEnabled && register) {
-      register(pathToFileURL(absPnpLoaderPath));
-    }
   }
 }
 
